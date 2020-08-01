225a7139b31d261cfdfd325c0160dc43156f04c6
Dmitri <56200546+Syritx@users.noreply.github.com>
2020-08-01 17:18:05 -0400

    added specification

--- Main.java
+++ Main.java
@@ -10,21 +10,25 @@ public class Main {
 
         Calendar cal = Calendar.getInstance();
 
+        // gets date
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
         int day = cal.get(Calendar.DAY_OF_MONTH);
 
+        // gets the time of day
         int hour = cal.get(Calendar.HOUR_OF_DAY);
         int minutes = cal.get(Calendar.MINUTE);
         String dayIndicator = "AM";
 
+        // sets indicator
         if (hour >= 12) dayIndicator = "PM";
         if (hour > 12) hour = hour-12;
 
         String minutesFormat = Integer.toString(minutes);
         if (minutes < 10) minutesFormat = "0"+minutes;
 
+        // creates a date format
         String dateFormat = months[month] + " / " + day + " / " + year + " at " + hour + ":" + minutesFormat + " " + dayIndicator;
-        System.out.println("your time is: "+dateFormat);
+        System.out.println("your date is: "+dateFormat);
     }
 }
